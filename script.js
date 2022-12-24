a902e63a5a1c55fe8266ca780ecd5e2a1d0c9e24
Ygor Combi <ycombi349@gmail.com>
2022-12-24 09:06:57 -0300

    inicio mobile

---
+++ script.js
@@ -0,0 +1,8 @@
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) nav.classList.add("active-header");
+  else nav.classList.remove("active-header");
+}
+window.addEventListener("scroll", scrollHeader);
