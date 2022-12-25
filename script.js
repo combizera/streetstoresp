c18eea8fe7a2a78f44c5bd21116d14dca1e1d35a
Ygor Combi <ycombi349@gmail.com>
2022-12-25 09:49:31 -0300

    menu mobile finalizado

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ function scrollHeader() {
   else nav.classList.remove("active-header");
 }
 window.addEventListener("scroll", scrollHeader);
+
+/*==================== HEADER MOBILE ====================*/
+
+function btnHamburguer() {
+  const btnMobile = document.getElementById("btn-mobile");
+
+  function toggleMenu(event) {
+    if (event.type === "touchstart") event.preventDefault();
+    const nav = document.querySelector(".header-nav");
+    nav.classList.toggle("active");
+    const active = nav.classList.contains("active");
+    event.currentTarget.setAttribute("aria-expanded", active);
+
+    if (active) {
+      event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+    } else {
+      event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+    }
+  }
+
+  btnMobile.addEventListener("click", toggleMenu);
+  btnMobile.addEventListener("touchstart", toggleMenu);
+}
+
+btnHamburguer();
