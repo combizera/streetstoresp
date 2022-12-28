ee0a9cad133a6e845c6c6e8c3b6ff60cf9e681a8
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-12-28 11:16:42 -0300

    feat: add min-height to hero section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,33 @@
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 function scrollHeader() {
-  const nav = document.getElementById("header");
+  const nav = document.getElementById('header');
   // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-  if (this.scrollY >= 50) nav.classList.add("active-header");
-  else nav.classList.remove("active-header");
+  if (this.scrollY >= 50) nav.classList.add('active-header');
+  else nav.classList.remove('active-header');
 }
-window.addEventListener("scroll", scrollHeader);
+window.addEventListener('scroll', scrollHeader);
 
 /*==================== HEADER MOBILE ====================*/
 
 function btnHamburguer() {
-  const btnMobile = document.getElementById("btn-mobile");
+  const btnMobile = document.getElementById('btn-mobile');
 
   function toggleMenu(event) {
-    if (event.type === "touchstart") event.preventDefault();
-    const nav = document.querySelector(".header-nav");
-    nav.classList.toggle("active");
-    const active = nav.classList.contains("active");
-    event.currentTarget.setAttribute("aria-expanded", active);
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.querySelector('.header-nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
 
     if (active) {
-      event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+      event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
     } else {
-      event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+      event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
     }
   }
 
-  btnMobile.addEventListener("click", toggleMenu);
-  btnMobile.addEventListener("touchstart", toggleMenu);
+  btnMobile.addEventListener('click', toggleMenu);
+  btnMobile.addEventListener('touchstart', toggleMenu);
 }
 
 btnHamburguer();
