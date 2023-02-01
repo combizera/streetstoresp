2c54764eb64cc6a5a13fc48788544d2370bdb926
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2023-02-01 13:51:29 -0300

    feat(footer): add social media custom fields

--- footer.php
+++ footer.php
@@ -1,11 +1,18 @@
       <footer>
         <img class="logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo.svg" alt="Logotipo StreetStore SP">
         <div class="footer-nav">
+          <?php $redes_sociais = get_field('redes_sociais'); ?>
           <ul>
-            <li><a target="_blank" href="https://www.facebook.com/sp.streetstore"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-facebook.svg" alt="Ícone do Facebook"></a></li>
-            <li><a target="_blank" href="https://www.instagram.com/streetstoresp/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-instagram.svg" alt="Ícone do Instagram"></a></li>
-            <li><a target="_blank" href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-twitter.svg" alt="Ícone do Twitter"></a></li>
-            <li><a target="_blank" href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-whatsapp.svg" alt="Ícone do Whatsapp"></a></li>
+            <?php foreach ($redes_sociais as $rede_social => $link): ?>
+              <li>
+                <a target="_blank" href="<?php echo empty($link) ? '#' : $link; ?>">
+                  <img
+                    src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-<?php echo $rede_social; ?>.svg"
+                    alt="Ícone do <?php echo ucwords($rede_social); ?>"
+                  />
+                </a>
+              </li>
+            <?php endforeach; ?>
           </ul>
         </div>
         <div class="footer-credits">
--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ remove_action('admin_print_styles', 'print_emoji_styles');
 add_action('wp_enqueue_scripts', function () {
   wp_enqueue_style('swiper-css', get_template_directory_uri() . '/assets/vendor/swiperjs/swiper-bundle.min.css');
   wp_enqueue_script('swiper-js', get_template_directory_uri() . '/assets/vendor/swiperjs/swiper-bundle.min.js', [], null, true);
-  wp_enqueue_style('main-css', get_template_directory_uri() . '/assets/css/style.css');
+  wp_enqueue_style('main-css', get_template_directory_uri() . '/assets/css/style.css', [], null);
   wp_enqueue_script('main-js', get_template_directory_uri() . '/assets/js/script.js', ['swiper-js'], null, true);
 });
 
